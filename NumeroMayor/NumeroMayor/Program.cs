14fd1c11ad1cc951f5c62a48959ad61664f6e5ba
AliciaEA <aliestrada@uamv.edu.ni>
2024-08-22 12:09:13 -0600

    Ejercicio 7, Mayor de tres numeros

---
+++ NumeroMayor/NumeroMayor/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace NumeroMayor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.WriteLine("Bienvenido, proporciona tres numeros y te indicaremos el mayor de ellos");
+            Console.Write("Primer Numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Segundo Numero: ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.Write("Tercer Numero: ");
+            num3 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine($"El numero mayor es: {num1}");
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine($"El numero mayor es: {num2}");
+            }
+            else
+            {
+                Console.WriteLine($"El numero mayor es: {num3}");
+            }
+
+        }
+
+    }
+
+}

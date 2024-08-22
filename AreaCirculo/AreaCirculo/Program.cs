c43823937ecd0408cbd89ed5b0e07c740c94ec44
AliciaEA <aliestrada@uamv.edu.ni>
2024-08-22 12:07:02 -0600

    Ejercicio 4, Area de un Circulo

---
+++ AreaCirculo/AreaCirculo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AreaCirculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio;
+            double area;
+
+            Console.WriteLine("Ingrese el radio del círculo: ");
+            radio = Convert.ToDouble(Console.ReadLine());
+
+            area = Math.PI * Math.Pow(radio, 2);
+
+            Console.WriteLine($"El área del círculo es: {area}");
+        }
+    }
+}

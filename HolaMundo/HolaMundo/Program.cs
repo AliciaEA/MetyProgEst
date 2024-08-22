3f86d656261b09241f2721b83aff3c322c4e915a
AliciaEA <aliestrada@uamv.edu.ni>
2024-08-22 12:02:52 -0600

    Ejercicio 1, Hola Mundo

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HolaMundo
+ {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola desde la UAM");
+            Console.WriteLine("Mi nombre es Alicia");
+            Console.WriteLine("Tengo 16, soy estudiante de primer año de Ingeniería en Sistemas.");
+            Console.WriteLine("Soy parte de la comunidad de Artemis Devs");
+        }
+    }
+}

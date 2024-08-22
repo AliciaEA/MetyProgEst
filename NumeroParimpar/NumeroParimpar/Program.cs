4ddec18e30440e836853e4b73e73883d1f6a815d
AliciaEA <aliestrada@uamv.edu.ni>
2024-08-22 12:08:26 -0600

    Ejercicio 6, Numero par o impar

---
+++ NumeroParimpar/NumeroParimpar/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace NumeroParimpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.Write("Bienvenido, Proporciona un numero para saber si es par o impar: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine($"El numero {numero} es par");
+            }
+            else
+            {
+                Console.WriteLine($"El numero {numero} es impar");
+            }
+
+
+        }
+    }
+}

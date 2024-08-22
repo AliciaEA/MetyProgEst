46f51d14041d3892fc751225c4830e1d25933055
AliciaEA <aliestrada@uamv.edu.ni>
2024-08-22 12:07:52 -0600

    Ejercicio 5, Aumentar el Salario

---
+++ AumentoSalario/AumentoSalario/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AumentoSalario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, cargo;
+            double salarioAnterior, aumento, salarioFinal;
+
+            Console.Write("Ingrese el nombre del empleado: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingrese el cargo: ");
+            cargo = Console.ReadLine();
+            Console.Write("Ingrese el salario: ");
+            salarioAnterior = double.Parse(Console.ReadLine());
+
+            aumento = salarioAnterior * 0.1;
+
+            salarioFinal = salarioAnterior + aumento;
+
+            Console.WriteLine($"Nombre: {nombre}");
+            Console.WriteLine($"Cargo: {cargo}");
+            Console.WriteLine($"Salario anterior: {salarioAnterior}");
+            Console.WriteLine($"Aumento: {aumento}");
+            Console.WriteLine($"Salario final: {salarioFinal}");
+        }
+    }
+}

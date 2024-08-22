8964506969e188d1625bd1ff5e98825c3ab28b67
AliciaEA <aliestrada@uamv.edu.ni>
2024-08-22 12:04:38 -0600

    Ejercicio 2, Saludar

---
+++ Saludo/Saludo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Saludo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            //Leer el nombre y saludar
+            Console.Write("Bienvenido, proporciona tu nombre y apellido: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine($"Hola {nombre}, te mandamos un gran saludo");
+        }
+    }
+}

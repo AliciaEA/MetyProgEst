e5f5a1d750975548a09a7da70ff3676083e07154
AliciaEA <aliestrada@uamv.edu.ni>
2024-09-03 09:30:34 -0600

    Sesion 26/08/2024 - MayorDeEdad

---
+++ Sesion2/Sesion2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Sesion2.clases;
+
+namespace Sesion2
+{
+    internal class Program
+    {
+        //Leer los datos de una persona y decir si es mayor o menos de edad
+        static void Main(string[] args)
+        {
+            string nombre;
+            DateTime fechaNac;
+            Persona persona = new Persona();
+
+            Console.Write("Ingrese el nombre de la persona: ");
+            nombre = Console.ReadLine();
+            Console.Write("En que fecha naciste: ");
+            fechaNac = DateTime.Parse(Console.ReadLine());
+
+            persona.Nombre = nombre;
+            persona.FechaNac = fechaNac;
+
+            Console.WriteLine(persona.EvaluarEdad());
+
+        }
+    }
+}
